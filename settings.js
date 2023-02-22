62934ad7234b8eb5467105cdc6470ecda06ead97
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2023-02-22 13:56:40 +0700

    update

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.creator = 'Matchbox'// yourname
+global.MONGO_DB_URI = "mongodb+srv://adminpanel:Semarang20@botwa.x50enkc.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "BOTWA" //isi apa aja bebas
+global.your_email = "Stewardjason10@gmail.com" //email
+global.email_password = "Semarang20" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
